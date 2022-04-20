7d41115ea3cf31ce806b05401a79676f832eba51
Dale Bendixen <dalebendixen@gmail.com>
2022-04-19 18:12:15 -0600

    Swipe and Delete Favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -153,6 +153,12 @@ export const addFavorite = campsiteId => ({
     type: ActionTypes.ADD_FAVORITE,
     payload: campsiteId
 });
+
+export const deleteFavorite = campsiteId => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: campsiteId
+}); 
+
 export const addComment = comment => ({
     type: ActionTypes.ADD_COMMENT,
     payload: comment
