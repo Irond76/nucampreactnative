a0be54f2294a5d0766a51b9bc88b420afd9456da
Dale Bendixen <dalebendixen@gmail.com>
2022-04-21 18:41:49 -0600

    Animatable

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { postFavorite } from '../redux/ActionCreators';
 import { postComment } from '../redux/ActionCreators';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -23,38 +24,40 @@ function RenderCampsite(props) {
     const {campsite} = props;
     if (campsite) {
         return (
-            <Card 
-                featuredTitle={campsite.name}
-                image={{uri: baseUrl + campsite.image}}
+            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                <Card 
+                    featuredTitle={campsite.name}
+                    image={{uri: baseUrl + campsite.image}}
+                    
+                    >
                 
-                >
-            
-                <Text style={{margin: 10}}>
-                    {campsite.description}
-                </Text>
-                <View style={styles.cardRow}>
-                    <Icon
-                        name= {props.favorite ? 'heart' : 'heart-o'}
-                        type='font-awesome'
-                        color='#f50'
-                        raised
-                        reverse
-                        onPress={() => props.favorite ? 
-                        console.log('already marked as favorite') : props.markFavorite()}
-                        
-                    />
-                    <Icon
-                            name='pencil' 
+                    <Text style={{margin: 10}}>
+                        {campsite.description}
+                    </Text>
+                    <View style={styles.cardRow}>
+                        <Icon
+                            name= {props.favorite ? 'heart' : 'heart-o'}
                             type='font-awesome'
-                            color='#5637DD'
+                            color='#f50'
                             raised
                             reverse
-                            onPress={() => props.onShowModal()}
-                            
+                            onPress={() => props.favorite ? 
+                            console.log('already marked as favorite') : props.markFavorite()}
                             
                         />
-                </View>
-            </Card>
+                        <Icon
+                                name='pencil' 
+                                type='font-awesome'
+                                color='#5637DD'
+                                raised
+                                reverse
+                                onPress={() => props.onShowModal()}
+                                
+                                
+                            />
+                    </View>
+                </Card>
+            </Animatable.View>
         );
     }
     return <View />;
@@ -79,13 +82,15 @@ function RenderComments({comments}) {
     }
 
     return(
-        <Card title='Comments' >
-            <FlatList
-                data={comments}
-                renderItem={renderCommentItem}
-                keyExtractor={item => item.id.toString()}
-            />
-        </Card>
+        <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>
+            <Card title='Comments' >
+                <FlatList
+                    data={comments}
+                    renderItem={renderCommentItem}
+                    keyExtractor={item => item.id.toString()}
+                />
+            </Card>
+        </Animatable.View>
     )
 }
 
