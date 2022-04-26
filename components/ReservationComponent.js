30dbba251279841d33ce49c6dd32e7a7910930fb
Dale Bendixen <dalebendixen@gmail.com>
2022-04-26 10:29:14 -0600

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -3,6 +3,7 @@ import { Text, View, ScrollView, StyleSheet,
     Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
 
@@ -40,8 +41,11 @@ class Reservation extends Component {
                     style: 'cancel'
                 },
                 {
-                    text: 'OK',
-                    style: 'cancel'
+                    text: 'OK', 
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                        this.resetForm();
+                    }
                 }
             ],
             {cancelable: false}
@@ -57,6 +61,8 @@ class Reservation extends Component {
             });
         }
 
+        
+
         animate() {
             Animated.timing(
                 this.state.scaleValue,
@@ -68,6 +74,33 @@ class Reservation extends Component {
             ).start();
         }
     
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                 handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+    
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+    
+            let permissions = await Notifications.getPermissionsAsync();
+            if (!permissions.granted) {
+                permissions = await Notifications.requestPermissionsAsync();
+            }
+            if (permissions.granted) {
+                sendNotification();
+            }
+        }
+    
+           
 
     render() {
         return (
